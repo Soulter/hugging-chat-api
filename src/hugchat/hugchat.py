b589750fd1d52459842bd6023e348b3d90789a22
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-05-01 18:33:37 -0400

    added more padding in cli

--- src/hugchat/hugchat.py
+++ src/hugchat/hugchat.py
@@ -112,7 +112,7 @@ class ChatBot:
 
 def cli():
     chatbot = ChatBot()
-    print("-----HuggingChat-----")
+    print("-------HuggingChat-------")
     running = True
     while running:
         question = input("> ")
