cc33837a127fd5b4122dd6fbee4571a3bd8f3361
Rock Chin <1010553892@qq.com>
2023-06-08 10:10:58 +0800

    perf: add more error types

--- src/hugchat/exceptions.py
+++ src/hugchat/exceptions.py
@@ -1,3 +1,52 @@
 class ModelOverloadedError(Exception):
-    """Raised when hf return response `{"error":"Model is overloaded","error_type":"overloaded"}`"""
+    """
+    HF Model Overloaded Error
+    
+    Raised when hf return response `{"error":"Model is overloaded","error_type":"overloaded"}`
+    """
+    pass
+
+
+class ChatBotInitError(Exception):
+    """
+    ChatBot Init Error
+    
+    Raised when chatbot init failed
+    """
+    pass
+
+
+class CreateConversationError(Exception):
+    """
+    Create Conversation Error
+    
+    Raised when create conversation failed
+    """
+    pass
+
+
+class InvalidConversationIDError(Exception):
+    """
+    Invalid Conversation ID Error
+    
+    Raised when using a invalid conversation id
+    """
+    pass
+
+
+class DeleteConversationError(Exception):
+    """
+    Delete Conversation Error
+    
+    Raised when delete conversation failed
+    """
+    pass
+
+
+class ChatError(Exception):
+    """
+    Chat Error
+    
+    Raised when chat failed
+    """
     pass
