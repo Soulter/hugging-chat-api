b700e5933013f6927e93643ce837828048d36a61
ogios <2134692955@qq.com>
2023-10-01 14:02:52 +0800

    feat: add status check; const type var; next() process response;

--- src/hugchat/message.py
+++ src/hugchat/message.py
@@ -2,20 +2,47 @@ from dataclasses import  dataclass
 import time
 from typing import Generator, Union
 
+
+MSGTYPE_FINAL = "finalAnswer"
+MSGTYPE_STREAM = "stream"
+MSGTYPE_WEB = "webSearch"
+MSGTYPE_STATUS = "status"
+
+
 class WebSearchSource:
     title: str
     link: str
     hostname: str
 
+
 @dataclass
 class Message(Generator):
     g: Generator
     web_search: bool = False
-    web_search_sources: Union[list[WebSearchSource], None] = None
+    web_search_sources: list[WebSearchSource] = list()
     final_answer: Union[str, None] = None
+    web_search_done: bool = not web_search
+    stream_done: bool = False
 
     def __next__(self):
-        return next(self.g)
+        a: dict = next(self.g)
+        t: str = a["type"]
+        if t == MSGTYPE_FINAL:
+            self.final_answer = a["text"]
+            self.done = True
+        elif t == MSGTYPE_STREAM:
+            self.web_search_done = True
+        elif t == MSGTYPE_WEB:
+            self.web_search_sources = list()
+            sources = a['sources']
+            for source in sources:
+                wss = WebSearchSource()
+                wss.title = source['title']
+                wss.link = source['link']
+                wss.hostname = source['hostname']
+                self.web_search_sources.append(wss)
+            
+        return a
 
     def __iter__(self):
         return self
@@ -30,6 +57,7 @@ class Message(Generator):
 
     def send(self, __value):
         return self.g.send(__value)
+
 
 if __name__ == "__main__":
     def gen() -> Generator:
