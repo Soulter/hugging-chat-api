f7f00874d00735f4ce145b62061c7f262c2e07fc
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2023-10-14 19:12:13 -0600

    Add model class for future use
    Not sure if this is actually going to be used, but it is there if we want to do anything w/ it

--- src/hugchat/hugchat.py
+++ src/hugchat/hugchat.py
@@ -35,6 +35,46 @@ class conversation:
     def __str__(self) -> str:
         return self.id
 
+
+# For future use -- is not used currently
+class model:
+    def __init__(
+        self,
+        id: str = None,
+        name: str = None,
+        displayName: str = None,
+
+        preprompt: str = None,
+        promptExamples: list = None,
+        websiteUrl: str = None,
+        description: str = None,
+
+        datasetName: str = None,
+        datasetUrl: str = None,
+        modelUrl: str = None,
+        parameters: dict = None
+    ):
+        '''
+        Returns a model object
+        '''
+
+        self.id: str = id,
+        self.name: str = name,
+        self.displayName: str = displayName,
+
+        self.preprompt: str = preprompt,
+        self.promptExamples: list = promptExamples,
+        self.websiteUrl: str = websiteUrl,
+        self.description: str = description,
+
+        self.datasetName: str = datasetName,
+        self.datasetUrl: str = datasetUrl,
+        self.modelUrl: str = modelUrl,
+        self.parameters: dict = parameters
+
+    def __str__(self) -> str:
+        return self.id
+
 class ChatBot:
     cookies: dict
     """Cookies for authentication"""
