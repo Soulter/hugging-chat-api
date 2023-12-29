717ca10b46b87d966c21c24d384c8018bf04f04d
Soulter <37870767+Soulter@users.noreply.github.com>
2023-12-29 13:31:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="hugchat",
-    version="0.3.8",
+    version="0.3.9",
     description="A huggingchat python api.",
     long_description=open("README.md", "rt", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
