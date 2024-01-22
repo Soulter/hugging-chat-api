253d7aa2580d53bc969ef91b64ece3531046adf0
Akhil <92385307+akhil3417@users.noreply.github.com>
2024-01-22 11:52:30 +0530

    nit(cli): improve option help info

--- src/hugchat/cli.py
+++ src/hugchat/cli.py
@@ -47,8 +47,8 @@ def handle_command(chatbot: ChatBot, userInput: str) -> None:
 /switch <id>: Switches to the ID number or ID string passed.
 /switch: Shows a list of all conversations' info in *current session*. Then you can choose one to switch to.
 /switch all: Shows a list of all conversations' info in *your account*. Then you can choose one to switch to. (not recommended if your account has a lot of conversations)
-/prompt : Shows a list of predefined-prompts to choose from.
-/prompt id : Shows to a specific id (incase commited to memory).
+/prompt : Shows a list of available predefined system prompts to choose from.
+/prompt id : Chooses a specific system prompt by its ID.
 /del <id>: Deletes the ID number or ID string passed. Will not delete active session.
 /delete-all: Deletes all the conversations for the logged in user.
 /clear: Clear the terminal.
